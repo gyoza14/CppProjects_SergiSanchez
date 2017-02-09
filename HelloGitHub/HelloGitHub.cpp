421d54c88fa940e23a51102a79f1bc7bfd46d29f
sergisanchezsolares <sergisanchezsolares@S3-20>
2017-02-09 13:53:26 +0100

    Print HelloGit

---
+++ HelloGitHub/HelloGitHub.cpp
@@ -0,0 +1,15 @@
+// HelloGitHub.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	cout << "Hello GitHub" << endl;
+	system("pause");
+    return 0;
+}
+
