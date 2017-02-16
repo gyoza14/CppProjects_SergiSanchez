496eeaf3bd19dba12348280948591fdd9a7d3d90
sergisanchezsolares <sergisanchezsolares@S3-20>
2017-02-16 13:55:12 +0100

    Equisde

--- RandomEnemyGenerator/main.cpp
+++ RandomEnemyGenerator/main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
 
+enum EnemyType {zombie,vampire,ghost,witch};
+
+struct Enemy
+{
+	EnemyType type;
+	std::string name;
+	int healt;
+	EnemyType GetEnemyTypeString(){return type;}
+	Enemy CreateRandomEnemy()
+	{
+		
+	}
+};
+
 
 int main()
 {
-
+	
 	return 0;
 
 }
